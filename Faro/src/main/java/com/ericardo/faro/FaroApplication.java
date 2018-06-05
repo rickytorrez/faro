16ebda480a9f9ddfbb5c1fe3b9ee389615da44ed
rickytorrez <torrez.ricky@ymail.com>
2018-06-05 11:02:45 -0400

    first commit

---
+++ Faro/src/main/java/com/ericardo/faro/FaroApplication.java
@@ -0,0 +1,12 @@
+package com.ericardo.faro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaroApplication.class, args);
+	}
+}
