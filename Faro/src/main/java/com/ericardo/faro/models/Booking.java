91c15e78d0e23f1c05a85ba9de4c5aba47acbe5e
rickytorrez <torrez.ricky@ymail.com>
2018-06-13 14:59:22 -0400

    Booking Model

---
+++ Faro/src/main/java/com/ericardo/faro/models/Booking.java
@@ -0,0 +1,153 @@
+package com.ericardo.faro.models;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+public class Booking {
+
+	/*************************************** ATTRIBUTES **************************************/
+	
+	@Id
+	@GeneratedValue
+	private Long id;
+	 	
+	private String firstName;
+	
+	private String lastName;
+	
+	private String Email;
+	
+	private Integer sizeOfParty;
+	
+	private Long phone;
+	
+	private Date date;
+	
+	private String time;
+	
+	private String message;
+	
+	@DateTimeFormat(pattern="MM:dd:yyyy HH:mm:ss")
+	private Date createdAt;
+	 	
+	@DateTimeFormat(pattern="MM:dd:yyyy HH:mm:ss")
+	private Date updatedAt;
+
+	/********************************* PERSIST AND UPDATE  **********************************/
+	 	
+	@PrePersist
+	public void onCreate() {
+	 	this.createdAt = new Date();
+	}
+	 	
+	@PreUpdate
+	public void onUpdate() {
+	 	this.updatedAt = new Date();
+	}
+	
+	/******************************** SETTERS AND GETTERS  **********************************/
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return Email;
+	}
+
+	public void setEmail(String email) {
+		Email = email;
+	}
+
+	public Integer getSizeOfParty() {
+		return sizeOfParty;
+	}
+
+	public void setSizeOfParty(Integer sizeOfParty) {
+		this.sizeOfParty = sizeOfParty;
+	}
+
+	public Long getPhone() {
+		return phone;
+	}
+
+	public void setPhone(Long phone) {
+		this.phone = phone;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	/********************************** EMPTY CONSTRUCTOR  **********************************/
+	 	
+	public Booking() {
+	 	this.createdAt = new Date();
+	 	this.updatedAt = new Date();
+	}
+
+}
