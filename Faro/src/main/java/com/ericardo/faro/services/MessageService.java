0edcc1ee209834aa9619866c1e76e4ab9fc3a2cb
rickytorrez <torrez.ricky@ymail.com>
2018-06-20 16:43:33 -0400

    Message Service

--- Faro/src/main/java/com/ericardo/faro/services/MessageService.java
+++ Faro/src/main/java/com/ericardo/faro/services/MessageService.java
@@ -1,5 +1,37 @@
 package com.ericardo.faro.services;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ericardo.faro.models.Message;
+import com.ericardo.faro.repositories.MessageRepository;
+
+@Service
 public class MessageService {
 
+	@Autowired
+	private MessageRepository _mR;
+	
+	public void create(Message message) {
+		_mR.save(message);
+	}
+	
+	public Message find(Long id) {
+		return (Message) _mR.findOne(id);
+	}
+	
+	public void update(Message message) {
+		_mR.save(message);
+	}
+	
+	public void destroy(Long id) {
+		_mR.delete(id);
+	}
+	
+	public ArrayList<Message> all(){
+		return (ArrayList<Message>) _mR.findAll();
+	}
+	
 }
