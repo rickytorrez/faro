8c0a4fe150035af83e2b37ffece84931cb786395
rickytorrez <torrez.ricky@ymail.com>
2018-06-22 14:00:40 -0400

    Client Side

--- Faro/src/main/java/com/ericardo/faro/controllers/RouteController.java
+++ Faro/src/main/java/com/ericardo/faro/controllers/RouteController.java
@@ -1,33 +1,19 @@
 package com.ericardo.faro.controllers;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.ericardo.faro.services.AdminService;
-
 @Controller
-@RequestMapping("/**")
+@RequestMapping("/")
 public class RouteController {
 
-	@Autowired
-	private AdminService _aS;
-	
 	/************ ROUTE THAT TAKES YOU HOME OR DASHBOARD IF ADMIN IS IN SESSION **************/
 
-	@RequestMapping("")
-	public String index(HttpServletRequest _request, HttpSession _session, Model _model) {
-		if(_session.getAttribute("id") == null) {
-			
-			System.out.println("landing page on _rC1 ");
-			return "index";
-		} else {
-			return "redirect:/restaurant/dashboard";
-		}
+	@RequestMapping("/")
+	public String landingPage() {
+		return "index";
+
 	}
 	
 }
